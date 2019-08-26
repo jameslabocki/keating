e59f0a77261ec2c317561736492b84db4e0118b6
James Labocki <jameslabocki@unknown1856806ed24b.home>
2019-08-26 15:56:00 -0400

    Now with unique VM names on up to 10 hosts.

--- keating.sh
+++ keating.sh
@@ -18,8 +18,46 @@ cp mastertemplate.json keating.json
 
 while [ $i -lt $1 ]; 
 do 
+	a="${i}1"
+	b="${i}2"
+	c="${i}3"
+	d="${i}4"
+	e="${i}5"
+	f="${i}6"
+	g="${i}7"
+	h="${i}8"
+	j="${i}9"
 	echo "creating host${i}"
-	sed -e "s/IDENTIFIER/$i/" -e "s/SHORTNAME/host-$i/" -e "s/FQDN/host-$i.example.com/g" hosttemplate.json >> keating.json
+	sed -e "s/IDENTIFIER/$i/" \
+	    -e "s/SHORTNAME/host-$i/"  \
+	    -e "s/VMID1/$a/" \
+	    -e "s/VMNAME1/virtual-$a/" \
+	    -e "s/VMEMS1/vm-$a/" \
+	    -e "s/VMID2/$b/" \
+	    -e "s/VMNAME2/virtual-$b/" \
+	    -e "s/VMEMS2/vm-$b/" \
+	    -e "s/VMID3/$c/" \
+	    -e "s/VMNAME3/virtual-$c/" \
+	    -e "s/VMEMS3/vm-$c/" \
+	    -e "s/VMID4/$d/" \
+	    -e "s/VMNAME4/virtual-$d/" \
+	    -e "s/VMEMS4/vm-$d/" \
+	    -e "s/VMID5/$e/" \
+	    -e "s/VMNAME5/virtual-$e/" \
+	    -e "s/VMEMS5/vm-$e/" \
+	    -e "s/VMID6/$f/" \
+	    -e "s/VMNAME6/virtual-$f/" \
+	    -e "s/VMEMS6/vm-$f/" \
+	    -e "s/VMID7/$g/" \
+	    -e "s/VMNAME7/virtual-$g/" \
+	    -e "s/VMEMS7/vm-$g/" \
+	    -e "s/VMID8/$h/" \
+	    -e "s/VMNAME8/virtual-$h/" \
+	    -e "s/VMEMS8/vm-$h/" \
+	    -e "s/VMID9/$j/" \
+	    -e "s/VMNAME9/virtual-$j/" \
+	    -e "s/VMEMS9/vm-$j/" \
+            -e "s/FQDN/host-$i.example.com/g" hosttemplate.json >> keating.json
 	i=$[$i+1]
 done
 
